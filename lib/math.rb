6b8b7e00bc6bcb69d7b40cbe7a649984917aeab2
Darrian Soto <ddog350@gmail.com>
2019-09-04 05:25:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,9 +19,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  
+  remainder = num1 % num2
+  return remainder
 end
 
 def square_root(num)
-  
+  square_rooted_num = Math.sqrt(num)
+  return square_rooted_num
 end
